db6560191240c7b9f793b42b2f531605df226105
Paco <kitortola@gmail.com>
2021-11-30 19:01:41 +0100

    fi de seccio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
 
 import { HeroesModule } from './heroes/heros.module';
+import { ContadorModule } from './contador/contador/contador.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contador/contador/contador.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import { ContadorComponent } from './contador.component';
+
+@NgModule({
+    declarations: [
+        ContadorComponent
+    ],
+    imports: [],
+    exports: [
+        ContadorComponent
+    ]
+})
+
+export class ContadorModule {}
