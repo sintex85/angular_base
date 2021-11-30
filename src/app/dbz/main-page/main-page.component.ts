0a7d0da885e033d9cfc12f88124f533d25779aa6
Paco <kitortola@gmail.com>
2021-11-30 22:22:04 +0100

    mes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent{
 
+  personajes : Personaje[] = [
+    { nombre: 'Goku', poder: 9000 },
+    { nombre: 'Vegeta', poder: 8000 },
+    { nombre: 'Piccolo', poder: 7000 },
+    { nombre: 'Cell', poder: 6000 },
+  ];
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0,
+  }
+
   agregar(){
-    console.log('pepe');
+    if (this.nuevo.nombre.trim().length === 0) { return; }
+    
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+
+
   }
 }
