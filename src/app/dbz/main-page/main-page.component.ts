5feba1feea083caad732b9b362ac87c55c03d970
Paco <kitortola@gmail.com>
2021-12-01 21:08:46 +0100

    componentes hijos

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
 
-interface Personaje {
-  nombre: string;
-  poder: number;
-}
 
 @Component({
   selector: 'app-main-page',
